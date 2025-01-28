3974b784bfade764886bbfc0c1f7329478772834
George Mohan Bastiampillai <184592634+GMBastian@users.noreply.github.com>
2025-01-28 11:40:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.9 MB (24.92%)

--- Problems/0069-sqrtx/solution.cs
+++ Problems/0069-sqrtx/solution.cs
@@ -1,22 +1,23 @@
 public class Solution {
     public int MySqrt(int x) {
-        if (x == 0 || x == 1)
+        if (x == 1 || x == 0)
         return x;
-
+        // can use binary search to effectively use identify the mid value to identify the sqrt.
         int left = 1;
-        int right = x/2;
+        int right = x/2; // the x value can be halved to reduce the itenerations
 
         while (left <= right)
         {
-            int mid = left + (right-left)/2;
+            //identifying the mid and avoid overflowing
+            int mid = left + (right - left)/2;
             int sqrt = x/mid;
 
-            if (sqrt == mid)
-                return mid;
+            if(sqrt == mid)
+            return mid;
             else if(sqrt < mid)
-                right = mid -1;
+            right = mid -1;
             else
-                left = mid + 1;
+            left = mid +1;
         }
         return right;
     }
